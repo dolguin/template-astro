b7a4b97b52c94e7b726137684f1f2d6183c591e0
dolguin <dolguin@servel.cl>
2024-06-14 01:26:36 -0400

    style: :clown_face: formato standard
    se actualizan archivos para adaptarlos al formato standard

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import vue from "@astrojs/vue";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vue from '@astrojs/vue'
 
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
   }), vue()]
-});
+})
