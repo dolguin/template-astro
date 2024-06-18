e276d9ed941287ce66e7ce4ab9604b41fd89f456
dolguin <dolguin@servel.cl>
2024-06-17 20:47:04 -0400

    perf: :recycle: cambio uso variables de entorno
    se cambia el uso de variables de entorno a un archivo .ts para ser improtado en las páginas donde se requiere

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+// NOMBRE_SITIO= 'Template'
+// DESCRIPCION_SITIO= 'formato de template para proyectos'
+// VERSION_APP= '1.0.0'
+
+// DATABASE_URL='postgres://postgres:postgresql@localhost:5441/dbase'
+
+// DATOS DE LA APP
+export const NOMBRE_SITIO = import.meta.env.NOMBRE_SITIO || 'TEMPLATE'
+export const DESCRIPCION_SITIO = import.meta.env.DESCRIPCION_SITIO || 'Formato de template para proyectos'
+export const APP_VERSION = import.meta.env.APP_VERSION || '0.0.1'
+
+// DATA_BASE
+export const DATABASE_URL = import.meta.env.DATABASE_URL
